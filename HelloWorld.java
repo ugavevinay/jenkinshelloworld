9cabb87fc7862a806b4c0bdb20cff3e9ae470f1e
Vinay Ugwe <ugavevinay@gmail.com>
2022-02-04 15:58:01 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("First Jenkins Job");
+        System.out.println("First Jenkins Job java");
     }
 }
